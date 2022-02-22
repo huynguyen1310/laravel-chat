a99aa5e7ebc33ba527ceb2ec150894363cd57fe2
Huy Nguyen <huy.nguyen@gumiviet.com>
2022-02-22 17:37:37 +0700

    feat: add chat room

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ConversationController;
+use App\Http\Controllers\GroupController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +22,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('groups', GroupController::class);
+Route::resource('conversations', ConversationController::class);
